382d352f9a572753956806e769b54271e1e5a9ed
ArunSurase <73110841+ArunSurase@users.noreply.github.com>
2022-12-07 18:19:42 +0530

    Update ApiTest.java

--- src/main/java/com/demo/ApiTest.java
+++ src/main/java/com/demo/ApiTest.java
@@ -2,11 +2,12 @@ package com.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
+@RequestMapping("/test")
 public class ApiTest {
 
-	@GetMapping(value = "/apitest")
+	@GetMapping("/apitest")
 	public String testApi() {
 		return "msg from api demo";
 	}
